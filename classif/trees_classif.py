93680659c65169456d1f59c51685653a7afcd725
bulat15g <bulat15g@gmail.com>
2019-02-05 00:10:35 +0300

    added std of density

--- classif/trees_classif.py
+++ classif/trees_classif.py
@@ -10,15 +10,15 @@ fail_folder = os.getcwd() + "/fail/"
 min_mean = 0.6
 min_std = 0.1
 mid_of_max = 7
-side_of_square=0.1
+side_of_square = 0.1
 ###
-for i in [folder,fail_folder]:
+for i in [folder, fail_folder]:
     if not os.path.exists(i):
         os.makedirs(i)
 
 for file_name in glob.glob("*.txt"):
-    out_df = pd.DataFrame(columns=['rad', 'den_std'])
-
+    out_df = pd.DataFrame(columns=['rad', 'std', 'den_std'])
+    # shift coords
     df = pd.read_csv(file_name, delimiter=' ', header=None)
     df.columns = ['x', 'y', 'z', 'laser']
     df.drop('laser', 1, inplace=True)
@@ -26,23 +26,35 @@ for file_name in glob.glob("*.txt"):
     df['y'] -= df['y'].mean()
     df['z'] = (df['z'] - df['z'].min()) / (df['z'].max() - df['z'].min() + 0.001) * divs_count
     df['z'] = df['z'].round(0)
-
+    # count some params
     for num in range(int(divs_count)):
         ndf = df[df['z'] == num]
         ndf['x_t'] = np.round((ndf['x'] - ndf['x'].min()) / side_of_square)
         ndf['y_t'] = np.round((ndf['y'] - ndf['y'].min()) / side_of_square)
 
+        from collections import Counter
+        c = Counter(list(zip(ndf['x_t'], ndf['y_t'])))
+        b = list(c.values())
+        if ndf['x_t'].max() == ndf['x_t'].max():
+            for x in range(int(ndf['x_t'].max()) + 1):
+                for y in range(int(ndf['y_t'].max()) + 1):
+                    if (x, y) not in c:
+                        b.append(0)
+        else:b=[0]
+
         ndf['rad'] = np.sqrt(ndf['x'] ** 2 + ndf['y'] ** 2)
         a = ndf['rad'].tolist()
         a.sort()
 
-        out_df = out_df.append({"rad": np.median(a[-mid_of_max:]),'den_std':np.std(np.array([ndf['x_t'].values, ndf['y_t'].values]))}, ignore_index=True)
-
+        out_df = out_df.append(
+            {"rad": np.median(a[-mid_of_max:]), 'std': np.std(np.array([ndf['x_t'].values, ndf['y_t'].values])),
+             'den_std': np.std(b)},
+            ignore_index=True)
+    # out data
     out_df.fillna(method='ffill', inplace=True)
-
     out_fold = ''
     if out_df['rad'].mean() > min_mean and out_df['rad'].std() > min_std:
         out_fold = folder
     else:
         out_fold = fail_folder
-    out_df.to_csv(out_fold + file_name,sep='\t')
+    out_df.to_csv(out_fold + file_name, sep='\t')
